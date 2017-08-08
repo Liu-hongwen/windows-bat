c882f003e226be7c8a736fdba5168f16e8154e57
Liu-hongwen <Programmer_23@163.com>
2017-08-08 14:44:27 +0800

    Create convert_imageset_train.bat

---
+++ convert_imageset_train.bat
@@ -0,0 +1,8 @@
+C:\caffe-dev\caffe-master\Build\x64\Release\convert_imageset.exe ^
+--resize_height=128 --resize_width=128 ^
+--shuffle ^
+--backend="leveldb" ^
+C:\caffe-project\Age\ ^
+C:\caffe-project\Age\Age_label.txt ^
+C:\caffe-project\Age\age_caffenet\train\
+pause
