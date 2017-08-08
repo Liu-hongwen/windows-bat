713ea4c84e8288c971631fa295c72a0ef649543a
Liu-hongwen <Programmer_23@163.com>
2017-08-08 15:00:05 +0800

    Create train.bat

---
+++ train.bat
@@ -0,0 +1,4 @@
+C:\caffe-dev\caffe-master\Build\x64\Release\caffe.exe train ^
+-solver=C:/caffe-project/Age/age_caffenet/age_solver.prorotxt
+%-weights=C:/caffe-project/Race2_Classification/gender_net.caffemodel%
+pause
