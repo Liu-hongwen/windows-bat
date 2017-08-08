1b899b3fbba96937f7566db8c7cf7823651500b0
Liu-hongwen <Programmer_23@163.com>
2017-08-08 14:51:58 +0800

    Create convert_imageset_test.bat

---
+++ convert_imageset_test.bat
@@ -0,0 +1,9 @@
+C:\caffe-dev\caffe-master\Build\x64\Release\convert_imageset.exe ^
+--resize_height=128 --resize_width=128 ^
+--shuffle ^
+--backend="leveldb" ^
+C:\caffe-project\Age\test\ ^
+C:\caffe-project\Age\Age_test_label.txt ^
+C:\caffe-project\Age\age_caffenet\test\
+pause
+
