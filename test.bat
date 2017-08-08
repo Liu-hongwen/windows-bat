99b687e432ae712023c5c5acbafafbd43a59629b
Liu-hongwen <Programmer_23@163.com>
2017-08-08 15:02:08 +0800

    Create test.bat

---
+++ test.bat
@@ -0,0 +1,6 @@
+C:\caffe-dev\caffe-master\Build\x64\Release\caffe.exe test ^
+-model=C:/caffe-project/Age/age_caffenet/age_train_test.prototxt ^
+-weights=C:/caffe-project/Age/age_caffenet/models/_iter_500000.caffemodel
+%-gpu=0%
+pause
+
