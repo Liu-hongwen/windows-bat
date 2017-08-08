aef9150b2045721a0cdc5788a11e2cbe5e81a856
Liu-hongwen <Programmer_23@163.com>
2017-08-08 14:54:38 +0800

    Create compute_mean.bat

---
+++ compute_mean.bat
@@ -0,0 +1,5 @@
+C:\caffe-dev\caffe-master\Build\x64\Release\compute_image_mean.exe ^
+--backend=leveldb ^
+C:\caffe-project\Age\leveldb\train ^
+C:\caffe-project\Age\meanfile\mean.binaryproto
+pause
